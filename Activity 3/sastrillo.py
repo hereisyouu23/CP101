a38934db1f3d622bbd5f13cf8e1ec21745d3d1f5
hereisyouu23 <sastrillokiancaizer@gmail.com>
2024-09-29 11:15:01 +0800

    sastrillo.py

---
+++ Activity 3/sastrillo.py
@@ -0,0 +1,9 @@
+# Input hours worked and rate per hour
+hours = float(input("hours : "))
+rate = float(input("rate : "))
+
+# Calculate gross pay
+gross_pay = hours * rate
+
+# Output the gross pay
+print("Gross pay:", gross_pay)
