8d4f9e94008db5b26d3bcdf0b3dfc13d45918395
hereisyouu23 <sastrillokiancaizer@gmail.com>
2024-09-29 11:09:26 +0800

    sastrillo.py

---
+++ Activity 2/sastrillo.py
@@ -0,0 +1,10 @@
+# Input scores for each subject
+math = float(input("Math score: "))
+science = float(input("Science score: "))
+english = float(input("English score: "))
+
+# Calculate the average
+average = (math + science + english) / 3
+
+# Output the average
+print("Average score:", average)
